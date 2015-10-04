d58512dd3ed960136e310572dc3b959cd763c5ab
Mike Cornish <cornishmw@gmail.com>
2015-10-03 21:04:44 -0400

    Episode IDs

--- client/templates/track/track.js
+++ client/templates/track/track.js
@@ -1,11 +1,38 @@
 Template.track.onCreated(function() {
-
+    Session.set('choosing', true);
+    Session.set('tracking', false);
+    Session.set('episode', null);
+    Session.set('cast', null);
 });
 
 Template.track.helpers({
-
+    choosing: function() {
+        return Session.get('choosing');
+    },
+    tracking: function() {
+        return Session.get('tracking');
+    },
+    activeChars: function() {
+        if (Session.get('cast')) {
+            console.log(Session.get('cast'));
+            return Characters.find({name: {$in: Session.get('cast')} });
+        }
+    }
 });
 
 Template.track.events({
+    'click [data-hook=watch-here]': function() {
+        Session.set('choosing', false);
+    },
+    'click [data-hook=watch-else]': function() {
+        Session.set('choosing', false);
+    },
+    'change [name=episode]': function(e) {
+        const episodeNum = parseInt( $(e.target).val() );
+        const episode = Episodes.findOne({number: episodeNum});
 
+        Session.set('episode', episodeNum);
+        Session.set('cast', episode.cast);
+        Session.set('tracking', true);
+    }
 });
--- server/episodes_fixture.js
+++ server/episodes_fixture.js
@@ -5,175 +5,200 @@ if (Episodes.find().count() === 0) {
         name: 'Arrival at Kraghammer',
         cast: ['Vax', 'Vex', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
         // TODO Find original air dates
-        airDate: new Date(2015, 3, 13)
+        airDate: new Date(2015, 3, 13),
+        videoId: 'B-9ssjy33J4'
     });
 
     Episodes.insert({
         number: 2,
         name: 'Into the Greyspine Mines',
         cast: ['Vax', 'Vex', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 3, 20)
+        airDate: new Date(2015, 3, 20),
+        videoId: 'JTie0S_5gjE'
     });
 
     Episodes.insert({
         number: 3,
         name: 'Strange Bedfellows',
         cast: ['Vax', 'Vex', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 3, 27)
+        airDate: new Date(2015, 3, 27),
+        videoId: 'kpkCcb--r90'
     });
 
     Episodes.insert({
         number: 4,
         name: 'Attack on the Duergar Warcamp',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 4, 3)
+        airDate: new Date(2015, 4, 3),
+        videoId: 'kGxiZNbjwGI'
     });
 
     Episodes.insert({
         number: 5,
         name: 'The Trick About Falling',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius'],
-        airDate: new Date(2015, 4, 3)
+        airDate: new Date(2015, 4, 3),
+        videoId: 'u6QpXDL7E8Y'
     });
 
     Episodes.insert({
         number: 6,
         name: 'Breaching the Emberhold',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 4, 17)
+        airDate: new Date(2015, 4, 17),
+        videoId: 'cemCmD27Rnc'
     });
 
     Episodes.insert({
         number: 7,
         name: 'The Throne Room',
         cast: ['Vax', 'Vex', 'Pike', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 4, 24)
+        airDate: new Date(2015, 4, 24),
+        videoId: 'XGs3bOX4XFQ'
     });
 
     Episodes.insert({
         number: 8,
         name: 'Glass and Bone',
         cast: ['Vax', 'Vex', 'Pike', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 5, 1)
+        airDate: new Date(2015, 5, 1),
+        videoId: 'A14MzQxxdwM'
     });
 
     Episodes.insert({
         number: 9,
         name: 'Yug\'Voril Uncovered',
         cast: ['Vax', 'Vex', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 5, 8)
+        airDate: new Date(2015, 5, 8),
+        videoId: 'IsLIiKFSJLQ'
     });
 
     Episodes.insert({
         number: 10,
         name: 'K\'Varn Revealed',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 5, 15)
+        airDate: new Date(2015, 5, 15),
+        videoId: 'AvnaMU7Dr-E'
     });
 
     Episodes.insert({
         number: 11,
         name: 'The Temple Showdown',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 5, 22)
+        airDate: new Date(2015, 5, 22),
+        videoId: 'xoYyHYsl5po'
     });
 
     Episodes.insert({
         number: 12,
         name: 'Dungeons & Dragons Campaign Tips',
         cast: ['Vox Moronica'],
-        airDate: new Date(2015, 5, 29)
+        airDate: new Date(2015, 5, 29),
+        videoId: 'P8EcS0WYbuc'
     });
 
     Episodes.insert({
         number: 13,
         name: 'Escape From the Underdark',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 6, 8)
+        airDate: new Date(2015, 6, 8),
+        videoId: '315mZGX-mjs'
     });
 
     Episodes.insert({
         number: 14,
         name: 'Shopping and Shipping',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius'],
-        airDate: new Date(2015, 6, 15)
+        airDate: new Date(2015, 6, 15),
+        videoId: 'Z4tjssfei5E'
     });
 
     Episodes.insert({
         number: 15,
         name: 'Skyward',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 6, 29)
+        airDate: new Date(2015, 6, 29),
+        videoId: '_dIBh0dGhzI'
     });
 
     Episodes.insert({
         number: 16,
         name: 'Enter Vasselheim',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 7, 6)
+        airDate: new Date(2015, 7, 6),
+        videoId: 'elarLmuDtzU'
     });
 
     Episodes.insert({
         number: 17,
         name: 'Hubris',
         cast: ['Vax', 'Vex', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 7, 27)
+        airDate: new Date(2015, 7, 27),
+        videoId: 'QDdT5UqGaDw'
     });
 
     Episodes.insert({
         number: 18,
         name: 'Trial of the Take: Part 1',
         cast: ['Vex', 'Grog', 'Zahra', 'Percy', 'Lyra', 'Scanlan'],
-        airDate: new Date(2015, 8, 3)
+        airDate: new Date(2015, 8, 3),
+        videoId: '60sUkTh6xBc'
     });
 
     Episodes.insert({
         number: 19,
         name: 'Trial of the Take: Part 2',
         cast: ['Vex', 'Grog', 'Zahra', 'Percy', 'Lyra', 'Scanlan'],
-        airDate: new Date(2015, 8, 10)
+        airDate: new Date(2015, 8, 10),
+        videoId: 'IW6GgFQg3kk'
     });
 
     Episodes.insert({
         number: 20,
         name: 'Trial of the Take: Part 3',
         cast: ['Thorbir', 'Vax', 'Tiberius', 'Kashaw', 'Keyleth'],
-        airDate: new Date(2015, 8, 17)
+        airDate: new Date(2015, 8, 17),
+        videoId: 'GFU_qqwSs0U'
     });
 
     Episodes.insert({
         number: 21,
         name: 'Trial of the Take: Part 4',
         cast: ['Thorbir', 'Vax', 'Tiberius', 'Kashaw', 'Keyleth'],
-        airDate: new Date(2015, 8, 24)
+        airDate: new Date(2015, 8, 24),
+        videoId: 'zGaISHs2s7Q'
     });
 
     Episodes.insert({
         number: 22,
         name: 'Aramente to Pyrah',
         cast: ['Vax', 'Vex', 'Pike', 'Grog', 'Percy', 'Keyleth', 'Tiberius'],
-        airDate: new Date(2015, 8, 31)
+        airDate: new Date(2015, 8, 31),
+        videoId: '1GY3HTZE5R0'
     });
 
     Episodes.insert({
         number: 23,
         name: 'The Rematch',
         cast: ['Vax', 'Vex', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 9, 14)
+        airDate: new Date(2015, 9, 14),
+        videoId: 'FM6L4tuyXq0'
     });
 
     Episodes.insert({
         number: 24,
         name: 'The Feast',
         cast: ['Vax', 'Vex', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 9, 21)
+        airDate: new Date(2015, 9, 21),
+        videoId: 'kre27f30IVs'
     });
 
     Episodes.insert({
         number: 25,
         name: 'Crimson Diplomacy',
         cast: ['Vax', 'Vex', 'Lilith', 'Grog', 'Percy', 'Keyleth', 'Tiberius', 'Scanlan'],
-        airDate: new Date(2015, 9, 28)
+        airDate: new Date(2015, 9, 28),
+        videoId: 'yrrB_q-V8G0'
     });
 
 }
