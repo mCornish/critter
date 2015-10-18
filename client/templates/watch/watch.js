ac76824d7c250fdb534476be3d99d902df3c21c3
Mike Cornish <cornishmw@gmail.com>
2015-10-18 17:54:31 -0400

    Watch styles

--- client/templates/watch/watch.js
+++ client/templates/watch/watch.js
@@ -15,9 +15,12 @@ Template.watch.onCreated(function () {
     Session.set('epContent', null);
     Session.set('contentActive', false);
     Session.set('infoActive', false);
+    Session.set('watchActive', false);
+    Session.set('watchHere', false);
     Session.set('detailActive', false);
     Session.set('character', null);
     Session.set('menuActive', null);
+    Session.set('player', null);
 });
 
 Template.watch.helpers({
@@ -30,7 +33,7 @@ Template.watch.helpers({
     choosing: function () {
         return Session.get('choosing');
     },
-    selectedEp: function() {
+    selectedEp: function () {
         return Session.get('selectedEp');
     },
     videoId: function () {
@@ -58,6 +61,9 @@ Template.watch.helpers({
     infoActive: function () {
         return Session.get('infoActive');
     },
+    watchActive: function () {
+        return Session.get('watchActive') ? 'is-active' : '';
+    },
     detailActive: function () {
         return Session.get('detailActive');
     },
@@ -68,33 +74,37 @@ Template.watch.helpers({
         const activeName = Session.get('charName');
         return name === activeName ? 'is-active' : '';
     },
-    menuActive: function(item) {
+    menuActive: function (item) {
         return item === Session.get('menuActive') ? 'is-active' : '';
     }
 });
 
 Template.watch.events({
-    'click [data-hook=watch]': function() {
+    'click [data-hook=watch]': function () {
         Session.set('watching', true);
         Session.set('tracking', false);
         Session.set('choosing', true);
     },
-    'click [data-hook=track]': function() {
+    'click [data-hook=track]': function () {
         Session.set('tracking', true);
         Session.set('watching', false);
     },
-    'click [data-hook=watch-here]': function () {
-        Session.set('choosing', false);
-        Session.set('watchHere', true);
+    'click [data-hook=watch-button]': function () {
+        const isActive = Session.get('watchActive');
+        const player = Session.get('player');
+        Session.set('watchActive', !isActive);
+        Session.set('watchHere', !isActive);
+
+        // TODO Figure out how to pause video when button is pressed
     },
     'click [data-hook=watch-else]': function () {
         Session.set('choosing', false);
         Session.set('watchHere', false);
     },
-    'change [name=episode]': function (e) {
-        const episodeNum = parseInt($(e.target).val());
+    'click [data-hook=episode-button]': function (e) {
+        const episodeNum = parseInt($(e.target).attr('data-number'));
         const episode = Episodes.findOne({number: episodeNum});
-        const contentCursor = Content.find({episode: episodeNum}, {$sort: {hour: -1, minute: -1, second: -1}})
+        const contentCursor = Content.find({episode: episodeNum}, {$sort: {hour: -1, minute: -1, second: -1}});
         let content = contentCursor.fetch();
         let times = _.pluck(content, 'time');
 
@@ -113,101 +123,99 @@ Template.watch.events({
         Session.set('selectedEp', true);
 
 
-        if (Session.get('watchHere')) {
-            // Used ID because jQuery select wasn't working
-            const ytEl = document.getElementById('js-yt');
-            const player = youtube({el: ytEl, id: episode.videoId});
+        // Used ID because jQuery select wasn't working
+        const ytEl = document.getElementById('js-yt');
+        const player = youtube({el: ytEl, id: episode.videoId});
+        Session.set('player', player);
 
-            // Keep track of content and play at correct time
-            player.on('timeupdate', _.throttle(function () {
-                let totalSeconds = Math.floor(player.currentTime);
-                const hours = Math.floor(totalSeconds / 3600);
-                totalSeconds %= 3600;
-                let minutes = Math.floor(totalSeconds / 60);
-                let seconds = Math.floor(totalSeconds % 60);
+        // Keep track of content and play at correct time
+        player.on('timeupdate', _.throttle(function () {
+            let totalSeconds = Math.floor(player.currentTime);
+            const hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+            let minutes = Math.floor(totalSeconds / 60);
+            let seconds = Math.floor(totalSeconds % 60);
 
-                if (cTotSec === totalSeconds && !Session.get('resetting')) {
-                    $('iframe.twitter-tweet').remove();
-                    Session.set('epContent', content[0]);
-                    Session.set('contentType', content[0].type);
-                    // Remove first item from times & content, and reset times
-                    if (times.length > 1) {
-                        times.shift();
-                        content.shift();
-                        cHourToSec = times[0].hour * 3600;
-                        cMinToSec = times[0].minute * 60;
-                        cSec = times[0].second;
-                        cTotSec = cHourToSec + cMinToSec + cSec;
-                    }
+            if (cTotSec === totalSeconds && !Session.get('resetting')) {
+                $('iframe.twitter-tweet').remove();
+                Session.set('epContent', content[0]);
+                Session.set('contentType', content[0].type);
+                // Remove first item from times & content, and reset times
+                if (times.length > 1) {
+                    times.shift();
+                    content.shift();
+                    cHourToSec = times[0].hour * 3600;
+                    cMinToSec = times[0].minute * 60;
+                    cSec = times[0].second;
+                    cTotSec = cHourToSec + cMinToSec + cSec;
                 }
+            }
 
-                Session.set('seconds', seconds);
-                Session.set('minutes', minutes);
-                Session.set('hours', hours);
+            Session.set('seconds', seconds);
+            Session.set('minutes', minutes);
+            Session.set('hours', hours);
 
-                // add zero for single-digit seconds/minutes
-                if (seconds < 10) {
-                    seconds = ('0' + seconds).slice(-2);
+            // add zero for single-digit seconds/minutes
+            if (seconds < 10) {
+                seconds = ('0' + seconds).slice(-2);
+            }
+            if (minutes < 10) {
+                minutes = ('0' + minutes).slice(-2);
+            }
+
+            Session.set('duration', `${hours}:${minutes}:${seconds}`);
+        }, 500));
+
+        // Reset and update content on scrub/play
+        player.on('play', function (e) {
+            Session.set('resetting', true);
+            // Reset and update content and times after scrubbing.
+            content = contentCursor.fetch();
+            times = _.pluck(content, 'time');
+
+            cHourToSec = times[0].hour * 3600;
+            cMinToSec = times[0].minute * 60;
+            cSec = times[0].second;
+            cTotSec = cHourToSec + cMinToSec + cSec;
+
+            let totalSeconds = Math.floor(player.currentTime);
+            const hours = Math.floor(totalSeconds / 3600);
+            totalSeconds %= 3600;
+            let minutes = Math.floor(totalSeconds / 60);
+            let seconds = Math.floor(totalSeconds % 60);
+
+            //times.forEach(function(time) {
+            //    if (time.)
+            //});
+
+            while (cTotSec < totalSeconds) {
+                console.log(content);
+                if (times.length > 1) {
+                    times.shift();
+                    content.shift();
+                    cHourToSec = times[0].hour * 3600;
+                    cMinToSec = times[0].minute * 60;
+                    cSec = times[0].second;
+                    cTotSec = cHourToSec + cMinToSec + cSec;
+                } else {
+                    break;
                 }
-                if (minutes < 10) {
-                    minutes = ('0' + minutes).slice(-2);
-                }
+            }
+            Session.set('resetting', false);
+        })
 
-                Session.set('duration', `${hours}:${minutes}:${seconds}`);
-            }, 500));
-
-            // Reset and update content on scrub/play
-            player.on('play', function (e) {
-                Session.set('resetting', true);
-                console.log('reset');
-                // Reset and update content and times after scrubbing.
-                content = contentCursor.fetch();
-                times = _.pluck(content, 'time');
-
-                cHourToSec = times[0].hour * 3600;
-                cMinToSec = times[0].minute * 60;
-                cSec = times[0].second;
-                cTotSec = cHourToSec + cMinToSec + cSec;
-
-                let totalSeconds = Math.floor(player.currentTime);
-                const hours = Math.floor(totalSeconds / 3600);
-                totalSeconds %= 3600;
-                let minutes = Math.floor(totalSeconds / 60);
-                let seconds = Math.floor(totalSeconds % 60);
-
-                //times.forEach(function(time) {
-                //    if (time.)
-                //});
-
-                while (cTotSec < totalSeconds) {
-                    console.log(content);
-                    if (times.length > 1) {
-                        times.shift();
-                        content.shift();
-                        cHourToSec = times[0].hour * 3600;
-                        cMinToSec = times[0].minute * 60;
-                        cSec = times[0].second;
-                        cTotSec = cHourToSec + cMinToSec + cSec;
-                    } else {
-                        break;
-                    }
-                }
-                Session.set('resetting', false);
-            })
-
-        }
     },
-    'click [data-hook=content-button]': function() {
+    'click [data-hook=content-button]': function () {
         Session.set('contentActive', true);
         Session.set('infoActive', false);
         Session.set('menuActive', 'content');
     },
-    'click [data-hook=info-button]': function() {
+    'click [data-hook=info-button]': function () {
         Session.set('infoActive', true);
         Session.set('contentActive', false);
         Session.set('menuActive', 'info');
     },
-    'click [data-hook=detail-button]': function(e) {
+    'click [data-hook=detail-button]': function (e) {
         const charName = $(e.target).attr('data-name');
         Session.set('detailActive', true);
         const character = Characters.findOne({name: charName});
