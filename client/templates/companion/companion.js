49cd5e4d62b9a94d3058b1cdc37889a366f5495e
Mike Cornish <cornishmw@gmail.com>
2015-11-05 15:20:15 -0500

    Mixpanel

--- client/templates/companion/companion.js
+++ client/templates/companion/companion.js
@@ -1,4 +1,5 @@
 Template.companion.onCreated(function () {
+    Session.set('durationIsPos', false);
     const
         now = new Date(),
         offset = -8,
@@ -13,26 +14,60 @@ Template.companion.onCreated(function () {
         minutes = pstMinutes,
         seconds = pstSeconds;
 
+    if (hours < 0) {
+        seconds = 60 - seconds;
+        minutes = 60 - minutes;
+        hours = -hours;
+    }
     Session.set('duration', `${hours}:${minutes}:${seconds}`);
 
     const interval = setInterval(function () {
-        seconds++;
+        // Check whether it should count down (before show) or down (during show)
+        if (pstHours - showTime > 0) {
+            Session.set('durationIsPos', true);
+            seconds++;
 
-        if (seconds >= 60) {
-            minutes++;
-            if (minutes >= 60) {
-                hours++;
-                minutes = 0;
+            if (seconds >= 60) {
+                minutes++;
+                if (minutes >= 60) {
+                    hours++;
+                    minutes = 0;
+                }
+                seconds = 0;
             }
-            seconds = 0;
-        }
 
-        // add zero for single-digit seconds/minutes
-        if (seconds < 10) {
-            seconds = ('0' + seconds).slice(-2);
-        }
-        if (minutes < 10) {
-            minutes = ('0' + minutes).slice(-2);
+            // add zero for single-digit seconds/minutes
+            if (seconds < 10) {
+                seconds = ('0' + seconds).slice(-2);
+            }
+            if (minutes < 10) {
+                minutes = ('0' + minutes).slice(-2);
+            }
+        } else {
+            Session.set('durationIsPos', false);
+            if (hours < 0) {
+                seconds = 60 - seconds;
+                minutes = 60 - minutes;
+                hours = -hours;
+            }
+            seconds--;
+
+            if (seconds <= 0) {
+                minutes--;
+                if (minutes <= 0) {
+                    hours--;
+                    minutes = 59;
+                }
+                seconds = 59;
+            }
+
+            // add zero for single-digit seconds/minutes
+            if (seconds < 10) {
+                seconds = ('0' + seconds).slice(-2);
+            }
+            if (minutes < 10) {
+                minutes = ('0' + minutes).slice(-2);
+            }
         }
 
         Session.set('duration', `${hours}:${minutes}:${seconds}`);
@@ -47,18 +82,21 @@ Template.companion.onCreated(function () {
 });
 
 Template.companion.helpers({
-    isLive: function() {
+    isLive: function () {
         const stream = this.stream;
         return stream.live;
     },
-    liveContent: function() {
+    liveContent: function () {
         return $.parseHTML(this.stream.liveContent);
     },
-    contentType: function(type) {
+    contentType: function (type) {
         if (typeof this.stream.liveContent === 'object') {
             return type === this.stream.liveContent.type;
         }
         return '';
+    },
+    durationText: function () {
+        return Session.get('durationIsPos') ? 'Duration' : 'Countdown';
     },
     duration: function () {
         return Session.get('duration');
@@ -82,10 +120,10 @@ Template.companion.helpers({
     giveawayActive: function () {
         return Session.get('giveawayActive');
     },
-    subCount: function() {
+    subCount: function () {
         return this.stream.subCount;
     },
-    subsLeft: function() {
+    subsLeft: function () {
         const stream = this.stream;
         return stream.subGoal - stream.subCount;
     },
@@ -100,18 +138,39 @@ Template.companion.events({
         Session.set('infoActive', false);
         Session.set('giveawayActive', false);
         Session.set('menuActive', 'content');
+
+        const stream = Template.currentData();
+        let type = '';
+        if (typeof this.stream.liveContent === 'object') {
+            type = this.stream.liveContent.type;
+        }
+        mixpanel.track('Content button click', {contentType: type});
     },
     'click [data-hook=info-button]': function () {
         Session.set('infoActive', true);
         Session.set('contentActive', false);
         Session.set('giveawayActive', false);
         Session.set('menuActive', 'info');
+
+        const stream = Template.currentData();
+        let type = '';
+        if (typeof this.stream.liveContent === 'object') {
+            type = this.stream.liveContent.type;
+        }
+        mixpanel.track('Info button click', {contentType: type});
     },
     'click [data-hook=giveaway-button]': function () {
         Session.set('infoActive', false);
         Session.set('contentActive', false);
         Session.set('giveawayActive', true);
         Session.set('menuActive', 'giveaway');
+
+        const stream = Template.currentData();
+        let type = '';
+        if (typeof this.stream.liveContent === 'object') {
+            type = this.stream.liveContent.type;
+        }
+        mixpanel.track('Giveaway button click', {contentType: type});
     },
     'click [data-hook=detail-button]': function (e) {
         Session.set('detailActive', true);
@@ -119,5 +178,7 @@ Template.companion.events({
         Session.set('charName', charName);
         const character = Characters.findOne({name: charName});
         Session.set('character', character);
+
+        mixpanel.track('Character Detail button click', {character: charName});
     }
 });
